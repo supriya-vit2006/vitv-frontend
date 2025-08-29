ded9be1fd4ef63d983cfcaca9e3bbac1f929c61b
Supriya R Patil <supriya.rpatil2024@vitstudent.ac.in>
2025-08-29 18:13:27 +0530

    Updated backend URL and added CORS for frontend

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://vit-travel-buddy-v.onrender.com',
 });
 
 API.interceptors.request.use((config) => {
